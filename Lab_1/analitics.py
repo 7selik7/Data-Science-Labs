f4fc6c6d6afee881716b37c4e7b0100e7d65a313
Yevhen Seliutin <evgenkoli8@gmail.com>
2023-09-19 15:18:47 +0300

    finished task

--- Lab_1/analitics.py
+++ Lab_1/analitics.py
@@ -1,75 +1,117 @@
-# from scrapper import scrap_file
-# from datetime import datetime
-# import tkinter as tk
-# from tkinter import ttk
-# import matplotlib.pyplot as plt
-# from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-#
-# def show_chart():
-#     data = scrap_file(create_file=False)
-#
-#     dates = [datetime.strptime(d['date'], '%d.%m.%Y').date() for d in data]
-#     inflation_values = [float(d['actual_inflation'].replace('%', '').replace(',', '.')) for d in data]
-#     print(inflation_values)
-#
-#     root = tk.Tk()
-#     root.title("Dollar inflation chart")
-#
-#     fig, ax = plt.subplots(figsize=(8, 4))
-#     ax.plot(dates, inflation_values, marker='o', linestyle='-', color='black', markersize=3)
-#     ax.set_xlabel("Date")
-#     ax.set_ylabel("Inflation")
-#     plt.xticks(rotation=45)
-#     ax.grid(True)
-#
-#     canvas = FigureCanvasTkAgg(fig, master=root)
-#     canvas_widget = canvas.get_tk_widget()
-#     canvas_widget.pack()
-#
-#     root.mainloop()
-#
-# a = int(input())
-#
-# if a == 1:
-#     show_chart()
-# if a == 2:
-#     print('trend')
-
 import numpy as np
-from scrapper import scrap_file
-from datetime import datetime, timedelta
-import tkinter as tk
-import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from scrapper import scrap_file
+import matplotlib.pyplot as plt
+from datetime import datetime
+import tkinter as tk
 
-data = scrap_file(create_file=False)
-data = data[::-1]
-dates = [datetime.strptime(d['date'], '%d.%m.%Y').date() for d in data]
-inflation_values = [float(d['actual_inflation'].replace('%', '').replace(',', '.')) for d in data]
 
-root = tk.Tk()
-root.title("Dollar inflation chart with trend")
+def analyze_real_data(dates: list, real_inflation_values: list) -> None:
+    real_mean_inflation = np.mean(real_inflation_values)
+    real_std_deviation_inflation = np.std(real_inflation_values)
+    real_median_inflation = np.median(real_inflation_values)
+    real_min_inflation = np.min(real_inflation_values)
+    real_max_inflation = np.max(real_inflation_values)
+    real_variance_inflation = np.var(real_inflation_values)
 
-fig, ax = plt.subplots(figsize=(8, 4))
-ax.plot(dates, inflation_values, marker='o', linestyle='-', color='black', markersize=4)
-ax.set_xlabel("Date")
-ax.set_ylabel("Inflation")
-plt.xticks(rotation=45)
-ax.grid(True)
+    print("Real Data Statistics:")
+    print("Mean inflation value:", real_mean_inflation)
+    print("Standard deviation of inflation:", real_std_deviation_inflation)
+    print("Median inflation:", real_median_inflation)
+    print("Minimum inflation value:", real_min_inflation)
+    print("Maximum inflation value:", real_max_inflation)
+    print("Inflation variance:", real_variance_inflation)
 
-x_values = np.arange(len(dates))
+    fig, ax = plt.subplots(figsize=(8, 4))
+    ax.plot(dates, real_inflation_values, marker='o', linestyle='-', color='black', markersize=4)
+    ax.set_xlabel("Date")
+    ax.set_ylabel("Inflation")
+    plt.xticks(rotation=45)
+    ax.grid(True)
 
-degree = 15
-coefficients = np.polyfit(x_values, inflation_values, degree)
-polynomial = np.poly1d(coefficients)
-print(polynomial)
-trend_values = polynomial(x_values)
 
-ax.plot(dates, trend_values, linestyle='--', color='blue', label=f'Trend (degree={degree})')
-ax.legend()
+def generate_and_analyze_synthetic_data(dates: list, real_inflation_values: list) -> tuple:
+    degree = 15
+    noise_std = 0.5
 
-canvas = FigureCanvasTkAgg(fig, master=root)
-canvas_widget = canvas.get_tk_widget()
-canvas_widget.pack()
+    np.random.seed(0)
+    coefficients = np.polyfit(np.arange(len(dates)), real_inflation_values, degree)
+    synthetic_trend_values = np.polyval(coefficients, np.arange(len(dates)))
+    print("\nModel:")
+    print(np.poly1d(coefficients))
+    synthetic_inflation_values = synthetic_trend_values + np.random.normal(0, noise_std, len(dates))
 
-root.mainloop()
+    # Use this if you want to create anomaly
+    # anomaly_index = 20
+    # anomaly_value = 10.0
+    # synthetic_inflation_values[anomaly_index] = anomaly_value
+
+    synthetic_mean_inflation = np.mean(synthetic_inflation_values)
+    synthetic_std_deviation_inflation = np.std(synthetic_inflation_values)
+    synthetic_median_inflation = np.median(synthetic_inflation_values)
+    synthetic_min_inflation = np.min(synthetic_inflation_values)
+    synthetic_max_inflation = np.max(synthetic_inflation_values)
+    synthetic_variance_inflation = np.var(synthetic_inflation_values)
+
+    print("\nSynthetic Data Statistics:")
+    print("Mean inflation value:", synthetic_mean_inflation)
+    print("Standard deviation of inflation:", synthetic_std_deviation_inflation)
+    print("Median inflation:", synthetic_median_inflation)
+    print("Minimum inflation value:", synthetic_min_inflation)
+    print("Maximum inflation value:", synthetic_max_inflation)
+    print("Inflation variance:", synthetic_variance_inflation)
+
+    return synthetic_trend_values, synthetic_inflation_values
+
+
+if __name__ == '__main__':
+    data = scrap_file(create_file=False)
+    data = data[::-1]
+    parsed_dates = [datetime.strptime(d['date'], '%d.%m.%Y').date() for d in data]
+    parsed_inflation_values = [float(d['actual_inflation'].replace('%', '').replace(',', '.')) for d in data]
+
+    root = tk.Tk()
+    root.title("Dollar inflation chart with trend")
+
+    frame1 = tk.Frame(root)
+    frame1.pack()
+
+    analyze_real_data(dates=parsed_dates, real_inflation_values=parsed_inflation_values)
+
+    fig1, ax1 = plt.subplots(figsize=(8, 4))
+    ax1.plot(parsed_dates, parsed_inflation_values, marker='o', linestyle='-', color='black', markersize=4,
+             label='Parsed Data')
+    ax1.set_xlabel("Date")
+    ax1.set_ylabel("Inflation")
+    plt.xticks(rotation=45)
+    ax1.grid(True)
+    plt.legend()
+    plt.title("Dollar Inflation Chart (Parsed)")
+
+    canvas1 = FigureCanvasTkAgg(fig1, master=frame1)
+    canvas_widget1 = canvas1.get_tk_widget()
+    canvas_widget1.pack()
+
+    frame2 = tk.Frame(root)
+    frame2.pack()
+
+    synthetic_trend_values, synthetic_inflation_values = generate_and_analyze_synthetic_data(
+        dates=parsed_dates,
+        real_inflation_values=parsed_inflation_values)
+
+    fig2, ax2 = plt.subplots(figsize=(8, 4))
+    ax2.plot(parsed_dates, synthetic_inflation_values, marker='x', linestyle='--', color='red', markersize=4,
+             label='Synthetic Data')
+    ax2.plot(parsed_dates, synthetic_trend_values, linestyle='--', color='blue', label='Synthetic Trend')
+    ax2.set_xlabel("Date")
+    ax2.set_ylabel("Inflation")
+    plt.xticks(rotation=45)
+    ax2.grid(True)
+    plt.legend()
+    plt.title("Dollar Inflation Chart (Synthetic)")
+
+    canvas2 = FigureCanvasTkAgg(fig2, master=frame2)
+    canvas_widget2 = canvas2.get_tk_widget()
+    canvas_widget2.pack()
+
+    root.mainloop()
